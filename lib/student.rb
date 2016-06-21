a5f3c0416c4fe731d21428d805f7219ddfee9fbb
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-20 20:31:18 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,25 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = "SELECT * FROM students;"
+    rows = DB[:conn].execute(sql)
+    rows.map { |row| self.new_from_db(row) }
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = ?;"
+    row = DB[:conn].execute(sql, name)
+    self.new_from_db(row[0])
   end
   
   def save
@@ -39,5 +46,31 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = 9;"
+    row = DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12;"
+    row = DB[:conn].execute(sql)
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?;"
+    row = DB[:conn].execute(sql, x)
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1;"
+    row = DB[:conn].execute(sql)
+    new_from_db(row[0])
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students WHERE grade = ?;"
+    row = DB[:conn].execute(sql, x)
   end
 end
